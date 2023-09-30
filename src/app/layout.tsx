25b772355bf5e64da25445a876e8d184584015f2
Anthony Salinas Suarez <anthonysalinas78251@berkeley.edu>
2023-09-30 11:51:58 -0700

    Initial website commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
+import './main-page.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from "next/link"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className = "nav-header">
+          <nav>
+            <ul>
+              <li><a href="https://cs61c.org/fa23/" target = "_blank">Course Website</a></li>
+              <li><Link href = "/weekly-materials">Weekly Materials</Link></li>
+              <li><Link href = "/">Home</Link></li>
+              <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdUmRuEBrzXZeyEpAEF3qEWyKe6_xZ0mhQ0YXZ5ZlSpSy3kOA/viewform?usp=sf_link" target = "_blank">Feedback</a></li>
+              <li><a href ="/additional-resources">Additional Resources</a></li>
+            </ul>
+          </nav>
+        </div>
+        {children}</body>
     </html>
   )
 }
---
+++ src/app/weekly-materials/page.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className = "materials-container">
+        <p className = "weekly-title">Weekly Materials</p>
+        <div className = "weekly-materials-description">
+          <p>
+            Below you will find all of our slides along with their corresponding worksheet. We will keep updating this page with updated slides/worksheet links as the semester goes on!
+          </p>
+        </div>
+        <div className = "discussion-week-container">
+          <div className = "week-number">
+            <p>Discussion 1:&nbsp;</p>
+          </div>
+          <div className = "slides-redirect">
+            <a href = "https://docs.google.com/presentation/d/1TpazA04uEbRH1mzfkLG5w-AoP4xAcPji4ereS2kLd6U/edit?usp=drive_link" target = "_blank">
+              <p>Slides &#8599;</p>
+            </a>
+          </div>
+          <div className = "worksheet-redirect">
+            <a href = "https://cs61c.org/fa23/discussions/disc01/" target = "_blank">
+              <p>Worksheet &#8599;</p>
+            </a>
+          </div>
+        </div>
+        <div className = "discussion-week-container">
+          <div className = "week-number">
+            <p>Discussion 2:&nbsp;</p>
+          </div>
+          <div className = "slides-redirect">
+            <a href = "https://docs.google.com/presentation/d/1zK5j3WSZUIkEnAkj4y5CiUYdsjO9zG3PqFtlxaJhUQw/edit?usp=drive_link" target = "_blank">
+              <p>Slides &#8599;</p>
+            </a>
+          </div>
+          <div className = "worksheet-redirect">
+            <a href = "https://cs61c.org/fa23/discussions/disc02/" target = "_blank">
+              <p>Worksheet &#8599;</p>
+            </a>
+          </div>
+        </div>
+        <div className = "discussion-week-container">
+          <div className = "week-number">
+            <p>Discussion 3:&nbsp;</p>
+          </div>
+          <div className = "slides-redirect">
+            <a href = "https://docs.google.com/presentation/d/1QTe5RFZBAo3hayke282DJrllanqQCs6ADR77-smW1r4/edit?usp=drive_link" target = "_blank">
+              <p>Slides &#8599;</p>
+            </a>
+          </div>
+          <div className = "worksheet-redirect">
+            <a href = "https://cs61c.org/fa23/discussions/disc03/" target = "_blank">
+              <p>Worksheet &#8599;</p>
+            </a>
+          </div>
+        </div>
+        <div className = "discussion-week-container">
+          <div className = "week-number">
+            <p>Discussion 4:&nbsp;</p>
+          </div>
+          <div className = "slides-redirect">
+            <a href = "https://docs.google.com/presentation/d/1phsiBCzVai2-nlde5s3EyA5XoflCYzbHbGc8FgAHsxc/edit?usp=drive_link" target = "_blank">
+              <p>Slides &#8599;</p>
+            </a>
+          </div>
+          <div className = "worksheet-redirect">
+            <a href = "https://cs61c.org/fa23/discussions/disc04/" target = "_blank">
+              <p>Worksheet &#8599;</p>
+            </a>
+          </div>
+        </div>
+        <div className = "discussion-week-container">
+          <div className = "week-number">
+            <p>Discussion 5:&nbsp;</p>
+          </div>
+          <div className = "slides-redirect">
+            <a href = "https://docs.google.com/presentation/d/1IYzg9S0uQd_vhlbhusVbL8cK6aYinNG40wO23Mw-3Pw/edit?usp=drive_link" target = "_blank">
+              <p>Slides &#8599;</p>
+            </a>
+          </div>
+          <div className = "worksheet-redirect">
+            <a href = "https://cs61c.org/fa23/discussions/disc05/" target = "_blank">
+              <p>Worksheet &#8599;</p>
+            </a>
+          </div>
+        </div>
+    </div>
+  )
+}
