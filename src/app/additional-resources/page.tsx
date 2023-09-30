3611ddab0d2c5706360b8c328ec034b505c9a059
Anthony Salinas Suarez <anthonysalinas78251@berkeley.edu>
2023-09-30 13:11:21 -0700

    I think I settled with a final font size though I am not entirely happy with the weird thing going on in the navbar. Also began working on additional resources page --> Task for this weekend (if time permits), make a compilation of additional resources to add to website

--- src/app/additional-resources/page.tsx
+++ src/app/additional-resources/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
+    <div className = "resources-container">
+      <p className = "resources-title">Additional Resources</p>
+      <div className = "additional-resources-description">
+        <p>
+          Below you will find a compilation of additional resources that hopefully help you all throughout your CS61C journey! Please note that some of these materials may not be officially endorsed/affiliated with CS61C. We will continue to add resources and update this page as the semester goes on!
+        </p>
+      </div>
       
     </div>
   )
